f1af8768719e0fe4b6b3a84bcd2db2381c6ccf02
Elvis Jakobsson <elvisjacobsson94@gmail.com>
2020-03-20 22:25:57 +0100

    use custom instances for Letter instead of derived ones
    Having custom instances for for example Eq makes more sense considering
    that we want Letter 2 to be functionally the same as Letter 28.

--- src/Cipher.hs
+++ src/Cipher.hs
@@ -1,24 +1,41 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Cipher where
 
 import           Data.Char
+import           Data.Maybe
 
 newtype Letter =
-  Letter Int deriving (Eq, Ord, Num, Real, Enum, Integral)
+  Letter Int
+  deriving (Num, Real, Integral)
+
+instance Eq Letter where
+    Letter x == Letter y = (x `mod` 26) == (y `mod` 26)
+
+instance Ord Letter where
+    compare (Letter x) (Letter y) = compare (x `mod` 26) (y `mod` 26)
+
+instance Enum Letter where
+    toEnum = Letter
+    fromEnum (Letter x) = x `mod` 26
 
 instance Show Letter where
   show = show . toChar
 
 type Message = [Letter]
+
 type Key = [Letter]
 
 -- UTILS
 
 fromString :: String -> Message
-fromString = map fromChar . filter isLetter
+fromString = mapMaybe fromChar
 
-fromChar :: Char -> Letter
-fromChar ch = Letter $ ord (toLower ch) - ord 'a'
+fromChar :: Char -> Maybe Letter
+fromChar ch
+  | ch `elem` letters = Just . Letter $ ord (toLower ch) - ord 'a'
+  | otherwise = Nothing
+  where letters = ['A' .. 'Z'] ++ ['a' .. 'z']
 
 toString :: Message -> String
 toString = map toChar
@@ -30,16 +47,16 @@ shift :: Int -> Letter -> Letter
 shift = (+) . Letter
 
 -- CIPHERS
-
 caesar :: Int -> Message -> Message
 caesar = map . shift
 
 uncaesar :: Int -> Message -> Message
 uncaesar = caesar . negate
 
-vigenere :: Key -> Message -> Message 
+vigenere :: Key -> Message -> Message
 vigenere = zipWith f . cycle
-    where f (Letter k) = shift k 
+  where
+    f (Letter k) = shift k
 
 unvigenere :: Key -> Message -> Message
 unvigenere = vigenere . map negate
