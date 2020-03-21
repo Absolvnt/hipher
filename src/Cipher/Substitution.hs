6775c9c4b6fb41c4c023d3730c24eda526360f54
Elvis Jakobsson <elvisjacobsson94@gmail.com>
2020-03-22 00:00:37 +0100

    add Atbash and Affine ciphers

--- src/Cipher/Substitution.hs
+++ src/Cipher/Substitution.hs
@@ -1,15 +1,44 @@
 module Cipher.Substitution where
 
-import           Cipher (Message, shift)
+import           Cipher     (Message, letter, numberOfLetters, shift, unLetter)
+import           Data.List  (elemIndex)
+import           Data.Maybe (fromJust)
 
+-- CAESAR
 caesar :: Int -> Message -> Message
 caesar = map . shift
 
 uncaesar :: Int -> Message -> Message
 uncaesar = caesar . negate
 
+-- ATBASH
 atbash :: Message -> Message
-atbash = undefined
+atbash = map $ (mirror !!) . unLetter
+  where
+    mirror = map letter $ reverse [0 .. numberOfLetters - 1]
 
 unatbash :: Message -> Message
 unatbash = atbash
+
+-- AFFINE
+newtype AlphabetCoprime =
+  AlphabetCoprime Int
+  deriving (Show)
+
+alphabetCoprime :: Int -> Maybe AlphabetCoprime
+alphabetCoprime a
+  | gcd a numberOfLetters == 1 = Just $ AlphabetCoprime a
+  | otherwise = Nothing
+
+affine :: AlphabetCoprime -> Int -> Message -> Message
+affine (AlphabetCoprime a) b = map (letter . conv . unLetter)
+  where
+    conv x = a * x + b
+
+unaffine :: AlphabetCoprime -> Int -> Message -> Message
+unaffine (AlphabetCoprime a) b = map (letter . conv . unLetter)
+  where
+    conv x = aInv * (x - b)
+    aInv = modInv a numberOfLetters
+    modInv a' m' =
+      fromJust . elemIndex 1 $ map (\n -> a' * n `mod` m') [0 .. m' - 1]
